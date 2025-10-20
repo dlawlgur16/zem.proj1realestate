c3814ecd83dc1f7e77c3a84f079bb7ec9a3c35b8
ji <dlawlgur28@gmail.com>
2025-10-20 18:38:07 +0900

    fix: remove unused Navigate import to resolve ESLint error

--- src/components/MainApp.js
+++ src/components/MainApp.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Navigate, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import CsvLoader from './CsvLoader/CsvLoader';
 import DataAnalysis from './DataAnalysis/DataAnalysis';
 import ReportGenerator from './ReportGenerator';
